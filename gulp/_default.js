008bea2ea3d8a7398ab835e63caded8268795dfd
Jonas Päckos <jpackos@gmail.com>
2022-02-23 08:41:17 +0100

    Setup Gulp basics

---
+++ gulp/_default.js
@@ -0,0 +1,28 @@
+/**
+ * DEFAULT TASK
+ *
+ * This is run with the '$ gulp' command
+ */
+
+
+/**
+ * Require Gulp and common functions
+ */
+
+const gulp = require('gulp');
+
+
+module.exports = function () {
+
+	gulp.task('default', function () {
+		console.log('Gulp had nothing to do today.');
+	});
+
+};
+
+
+
+
+
+
+

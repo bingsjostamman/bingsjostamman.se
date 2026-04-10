4a815a38e1a865e328bc128fd71e06455827db7f
Jonas Päckos <jpackos@hotmail.com>
2026-04-10 08:17:30 +0200

    Separate barnens allspelslåtar from the regular ones

--- src/editions/2025/allspel-content.js
+++ src/editions/2025/allspel-content.js
@@ -1,7 +1,15 @@
 export default {
   intro: `
-Allspel är stunden då gästande musiker samlas på stora scenen och spelar tillsammans.
+Allspelet som avslutar invigningsprogrammet på stora scenen är stunden då alla spelmän välkomnas upp på scenen för att tillsammans
+låta hundratals instrument ljuda i ett pärlband av låtar. Olle Moraeus har valt ut låtar från olika delar av Dalarna till årets allspel.
 
-Här hittar du årets allspelslåtar med länkar till noter (PDF) och ljudfiler (MP3).
+Liksom tidigare år har även Maria Ähdel valt ut en allsångsslåt, då vi tar ned instrumenten och sjunger tillsammans. Maria som också arrangerat
+allsångslåten kommer att leda sången vid allspelet, så välj din favoritstämma och sjung!
+
+Nedan hittar du årets låtar till allspelet med länkar till noter (PDF) och ljudfiler (MP3). Tillsammans får vi rösterna att eka över bergen!
+
   `,
+  barnensAllspel: `
+Även på **Barnens Bingsjö** har vi ett allspel – här hittar du låtarna med noter och ljudfiler.
+`,
 };

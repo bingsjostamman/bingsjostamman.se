229ae1f657b8822da901ed74f4e1524e34d94975
Jonas Päckos <jpackos@hotmail.com>
2026-04-21 08:18:53 +0200

    Menu and header WIP

--- src/_includes/components/menu/menu.stories.js
+++ src/_includes/components/menu/menu.stories.js
@@ -26,7 +26,7 @@ export const Default = () => `
 <span class="icon" aria-hidden="true">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" /></svg>
 </span>
-    <span class="u-visually-hidden">Meny</span>
+    <span class="c-menu__toggle-label">Meny</span>
   </a>
 
   <!-- Backdrop only used when JS is active -->

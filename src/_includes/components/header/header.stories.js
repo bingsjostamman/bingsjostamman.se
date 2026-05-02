d726e18f91b471dc53f902fedb7df08fc6dfe327
Jonas Päckos <jpackos@hotmail.com>
2026-05-02 17:18:48 +0200

    Update header

--- src/_includes/components/header/header.stories.js
+++ src/_includes/components/header/header.stories.js
@@ -25,11 +25,11 @@ export const Default = () => `<header class="c-header">
 <div data-menu class="c-menu">
   <!-- Starts as an <a> for no-JS fallback -->
   <a href="#main-nav" data-menu-toggle class="c-menu__toggle">
+    <span class="c-menu__toggle-label">Meny</span>
     
 <span class="icon" aria-hidden="true">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" /></svg>
 </span>
-    <span class="c-menu__toggle-label">Meny</span>
   </a>
 
   <!-- Backdrop only used when JS is active -->

49c3a24437e59ed0e8d704e6f896871b7b97b9d5
Jonas Päckos <jpackos@hotmail.com>
2026-05-12 15:08:18 +0200

    Allspel component and page updates

--- src/_includes/components/allspel-tunes/allspel-tunes.stories.js
+++ src/_includes/components/allspel-tunes/allspel-tunes.stories.js
@@ -9,39 +9,66 @@ export default {
 };
 
 export const Default = () => `
-<div class="c-allspel-tunes-wrap">
+<div class="c-tunes-wrap">
 
-  <ul class="c-allspel-tunes-tunes">
-    <li class="c-allspel-tunes-tune o-tile t-intent-highlight">
-      <div class="c-allspel-tunes-info">
-        <span class="c-allspel-tunes-name">Bingsjöpolskan</span>
-        <span class="c-allspel-tunes-note">Melodi + andrastämma</span>
+  <ul class="c-tunes-tunes u-list-unstyled">
+    <li class="c-tunes-tune o-tile t-intent-emphasis">
+      <div class="c-tunes-info">
+        <h3 class="c-tunes-name">Bingsjöpolskan</h3>
+        <span class="c-tunes-note">Melodi + andrastämma</span>
       </div>
-      <div class="c-allspel-tunes-media">
+      <div class="c-tunes-media">
 
-        <div>Noter</div>
-          <ul class="c-allspel-tunes-links">
-            <li><a href="https://example.com/bingsjopolskan-melodi.pdf">Melodi (PDF)</a></li>
-            <li><a href="https://example.com/bingsjopolskan-andrastamma.pdf">Andrastämma (PDF)</a></li>
+        <div class="c-tunes-media-label">Noter</div>
+          <ul class="c-tunes-links u-list-unstyled">
+            <li><a class="c-tunes-media-link" href="https://example.com/bingsjopolskan-melodi.pdf">
+<span class="icon" aria-hidden="true">
+  <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
+    <path d="M16,9H13V14.5A2.5,2.5 0 0,1 10.5,17A2.5,2.5 0 0,1 8,14.5A2.5,2.5 0 0,1 10.5,12C11.07,12 11.58,12.19 12,12.5V7H16V9M19,3A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3H19M5,5V19H19V5H5Z" />
+</svg>
+</span><span>Melodi (PDF)</span></a></li>
+            <li><a class="c-tunes-media-link" href="https://example.com/bingsjopolskan-andrastamma.pdf">
+<span class="icon" aria-hidden="true">
+  <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
+    <path d="M16,9H13V14.5A2.5,2.5 0 0,1 10.5,17A2.5,2.5 0 0,1 8,14.5A2.5,2.5 0 0,1 10.5,12C11.07,12 11.58,12.19 12,12.5V7H16V9M19,3A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3H19M5,5V19H19V5H5Z" />
+</svg>
+</span><span>Andrastämma (PDF)</span></a></li>
           </ul>
 
-        <div>Ljudfil</div>
-          <a href="https://example.com/bingsjopolskan-melodi.mp3">Melodi (MP3)</a>
+        <div class="c-tunes-media-label">Ljudinspelning</div>
+          <a class="c-tunes-media-link" href="https://example.com/bingsjopolskan-melodi.mp3">
+<span class="icon" aria-hidden="true">
+  <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
+<path d="M19,19H5V5H19M19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M10,8V16L15,12L10,8Z" /></svg>
+</span><span>Melodi (MP3)</span></a>
       </div>
     </li>
-    <li class="c-allspel-tunes-tune o-tile t-intent-highlight">
-      <div class="c-allspel-tunes-info">
-        <span class="c-allspel-tunes-name">Gammelvals</span>
+    <li class="c-tunes-tune o-tile t-intent-emphasis">
+      <div class="c-tunes-info">
+        <h3 class="c-tunes-name">Gammelvals</h3>
       </div>
-      <div class="c-allspel-tunes-media">
+      <div class="c-tunes-media">
 
-        <div>Noter</div>
-          <a href="https://example.com/gammelvals.pdf">Notblad (PDF)</a>
+        <div class="c-tunes-media-label">Noter</div>
+          <a class="c-tunes-media-link" href="https://example.com/gammelvals.pdf">
+<span class="icon c-tunes-media-icon" aria-hidden="true">
+  <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
+    <path d="M16,9H13V14.5A2.5,2.5 0 0,1 10.5,17A2.5,2.5 0 0,1 8,14.5A2.5,2.5 0 0,1 10.5,12C11.07,12 11.58,12.19 12,12.5V7H16V9M19,3A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3H19M5,5V19H19V5H5Z" />
+</svg>
+</span><span>Notblad (PDF)</span></a>
 
-        <div>Ljudfil</div>
-          <ul class="c-allspel-tunes-links">
-              <li><a href="https://example.com/gammelvals.mp3">Ljudfil (MP3)</a></li>
-              <li><a href="https://example.com/gammelvals-andrastamma.mp3">Andrastämma (MP3)</a></li>
+        <div class="c-tunes-media-label">Ljudinspelning</div>
+          <ul class="c-tunes-links u-list-unstyled">
+              <li><a class="c-tunes-media-link" href="https://example.com/gammelvals.mp3">
+<span class="icon" aria-hidden="true">
+  <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
+<path d="M19,19H5V5H19M19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M10,8V16L15,12L10,8Z" /></svg>
+</span><span>Ljudfil (MP3)</span></a></li>
+              <li><a class="c-tunes-media-link" href="https://example.com/gammelvals-andrastamma.mp3">
+<span class="icon" aria-hidden="true">
+  <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24">
+<path d="M19,19H5V5H19M19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M10,8V16L15,12L10,8Z" /></svg>
+</span><span>Andrastämma (MP3)</span></a></li>
           </ul>
       </div>
     </li>
--- src/editions/2026/allspel-content.js
+++ src/editions/2026/allspel-content.js
@@ -1,12 +1,9 @@
 export default {
   intro: `
-Det mäktiga Allspelet som avslutar invigningsprogrammet på stora scenen är stunden då alla spelmän välkomnas upp på scenen för att tillsammans
-låta hundratals instrument ljuda i ett pärlband av låtar. Olle Moraeus har valt ut låtar från olika delar av Dalarna till årets allspel.
+Allspelet som avslutar invigningsprogrammet på stora scenen är stunden då alla spelmän välkomnas upp på scenen för att tillsammans
+låta hundratals instrument ljuda i ett pärlband av låtar.
 
-Liksom tidigare år har även Maria Ähdel valt ut en allsångsslåt, då vi tar ned instrumenten och sjunger tillsammans. Maria som också arrangerat
-allsångslåten kommer att leda sången vid allspelet, så välj din favoritstämma och sjung!
-
-Nedan hittar du årets låtar till allspelet med länkar till noter (PDF) och ljudfiler (MP3). Tillsammans får vi rösterna att eka över bergen!
+I år är det övergripande temat låtar från Bingsjö, och nedan hittar du allspelslåtarna med länkar till noter (PDF) och ljudfiler (MP3) att spela upp.
 
   `,
   barnensAllspel: `

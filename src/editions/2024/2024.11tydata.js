826d36e38eee3fe723f3192e8be34c5d79555042
Jonas Päckos <jpackos@hotmail.com>
2026-05-19 10:43:28 +0200

    Clean unused pages

--- src/editions/2024/2024.11tydata.js
+++
@@ -1,8 +0,0 @@
-export default {
-  year: "2024",
-  start: "2024-07-04",
-  end: "2024-07-05",
-  eleventyComputed: {
-    isArchive: ({ year, editions }) => String(year) !== String(editions.current),
-  },
-};

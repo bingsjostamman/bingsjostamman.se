b432a3baea8dd6b02baaac5ea4c592dc93ea1fd6
Jonas Päckos <jpackos@hotmail.com>
2026-05-21 13:25:17 +0200

    housekeeping

--- src/editions/2025/2025.11tydata.js
+++
@@ -1,8 +0,0 @@
-export default {
-  year: "2025",
-  start: "2025-07-03",
-  end: "2025-07-04",
-  eleventyComputed: {
-    isArchive: ({ year, editions }) => String(year) !== String(editions.current),
-  },
-};

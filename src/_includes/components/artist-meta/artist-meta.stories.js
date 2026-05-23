786c4524b51e5b9a7e7a4fcb98f724e994574964
Jonas Päckos <jpackos@hotmail.com>
2026-05-23 21:21:45 +0200

    componentify artist-meta

---
+++ src/_includes/components/artist-meta/artist-meta.stories.js
@@ -0,0 +1,11 @@
+export default {
+  title: "Components/artist-meta",
+  parameters: {
+    docs: { description: { component: "" } },
+    status: "",
+  },
+  tags: [],
+};
+
+export const Default = () => ``;
+Default.storyName = "Default";

ee1ae8e27ad8e145cd4b59d5b2dc2217265779bb
Jonas Päckos <jpackos@hotmail.com>
2026-05-25 23:21:15 +0200

    Created and added teaser cards for the frontpage

---
+++ src/_includes/components/frontpage-teasers/frontpage-teasers.stories.js
@@ -0,0 +1,12 @@
+import './frontpage-teasers.css';
+export default {
+  title: "Components/frontpage-teasers",
+  parameters: {
+    docs: { description: { component: "" } },
+    status: "",
+  },
+  tags: [],
+};
+
+export const Default = () => ``;
+Default.storyName = "Default";
